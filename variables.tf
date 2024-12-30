649266457751c1e87d89fc1fc20d24779c1ac605
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2024-12-30 11:57:36 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,55 +1,36 @@
-###########################
-# Common vars
-###########################
 variable "environment" {
-  description = "Variable that defines the name of the environment."
+  description = "The name of the environment to deploy resources into, such as 'dev', 'test', or 'prod'."
   type        = string
-  default     = "dev"
-}
-variable "default_tags" {
-  description = "A mapping of tags to assign to the resource."
-  type        = map(any)
-  default = {
-    "ManagedByTerraform" = "True"
-  }
-}
-variable "region" {
-  description = "Region in which resources are deployed."
-  type        = string
-  default     = "weu"
 }
 
-###########################
-# Resource groups vars
-###########################
+variable "default_tags" {
+  description = "A map of default tags to assign to all resources, aiding in resource organization and cost tracking."
+  type        = map(string)
+}
+
 variable "resource_group_location" {
-  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
-  default     = "West Europe"
+  description = "The Azure region where the resource group is located. Changing this value will recreate the Container Registry."
   type        = string
 }
+
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the container registry component. Changing this forces a new resource to be created."
+  description = "The name of the resource group in which the Container Registry will be created. Modifying this value will recreate the resource."
   type        = string
 }
-############################
-#Container registry variable
-############################
+
 variable "acr_tier" {
-  description = "The SKU name of the container registry. Possible values are Basic, Standard and Premium. Classic (which was previously Basic) is supported only for existing resources."
+  description = "The pricing tier (SKU) of the Azure Container Registry. Valid options are 'Basic', 'Standard', and 'Premium'. Default is 'Basic'."
   type        = string
   default     = "Basic"
+}
 
-  validation {
-    condition     = can(regex("^(Basic|Premium)$", var.acr_tier))
-    error_message = "Invalid value for acr_tier. Valid values are Basic or Premium."
-  }
-}
 variable "acr_name" {
-  description = "Specifies the name of the Container Registry. Changing this forces a new resource to be created."
+  description = "The name of the Azure Container Registry. Modifying this value will recreate the resource."
   type        = string
 }
+
 variable "acr_admin_enabled" {
-  description = "Specifies whether the admin user is enabled. Defaults to false."
-  type        = string
-  default     = "false"
+  description = "A boolean flag indicating whether the admin user account for the Container Registry is enabled. Default is false."
+  type        = bool
+  default     = false
 }
