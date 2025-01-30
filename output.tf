f557b688a484eaf7d9e211b375ab7c95a422c2e9
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2025-01-30 12:06:07 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,23 +1,20 @@
 output "id" {
-  description = "The unique identifier (ID) of the Azure Container Registry instance."
+  description = "The ID of the Container Registry."
   value       = azurerm_container_registry.main.id
   sensitive   = false
 }
-
 output "login_server" {
-  description = "The login URL for the Azure Container Registry, which can be used for authentication."
+  description = "The URL that can be used to log into the container registry."
   value       = azurerm_container_registry.main.login_server
   sensitive   = false
 }
-
 output "admin_username" {
-  description = "The username for the Azure Container Registry admin account, if the admin account is enabled."
+  description = "The Username associated with the Container Registry Admin account - if the admin account is enabled."
   value       = azurerm_container_registry.main.admin_username
   sensitive   = true
 }
-
 output "admin_password" {
-  description = "The password for the Azure Container Registry admin account, if the admin account is enabled."
+  description = "The Password associated with the Container Registry Admin account - if the admin account is enabled."
   value       = azurerm_container_registry.main.admin_password
   sensitive   = true
 }
